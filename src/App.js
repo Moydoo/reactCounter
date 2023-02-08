007721ec55f26595c9c195c08adb5073832f11d9
Michal <m.stefanski98@gmail.com>
2023-02-09 00:01:50 +0100

    github upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+const STORAGE_KEY = 'counterApp.counter'
 
 function App() {
+  const [count, setCounter] = useState(0)
+
+  useEffect(() => {
+    const storagedCounts = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    if (storagedCounts) setCounter(storagedCounts)
+  }, [])
+
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(count))
+  }, [count])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="text-center my-5">{count}</h1>
+        <div className="buttons-center">
+          <button className="btn btn-success" onClick={() => setCounter(count + 1)}>Increment</button>
+          <button className="btn btn-danger" onClick={() => setCounter(count - 1)} disabled={count === 0}>Decrement</button>
+          <button className="btn btn-secondary" onClick={() => setCounter(0)} disabled={count === 0}>Reset</button>
+          <button className="btn btn-primary" onClick={() => setCounter(count + 10)}>Add 10</button>
+        </div>
+      </div>
     </div>
   );
 }
